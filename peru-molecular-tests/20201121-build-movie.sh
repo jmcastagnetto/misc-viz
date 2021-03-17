0794f515856ee19f890d788ffaecae21d658b854
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-03-17 08:27:40 -0500

    reorg

--- peru-molecular-tests/20201121-build-movie.sh
+++ peru-molecular-tests/20201121-build-movie.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+cd ..
 ffmpeg -framerate 1 -pattern_type glob -i 'tmp/pm*.png' -i "assets/_ghost_-_Reverie_(small_theme).mp3" -c:v libx264 -preset slow -vf format=yuv420p movies/pm-cobertura-prov-weekly.mp4 -y
 
