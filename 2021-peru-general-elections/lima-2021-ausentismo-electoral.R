9665da88c86cc0d593a6e8c901f462f84f18e799
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-04-12 18:12:55 -0500

    ausentismo en lima dpto y prov

---
+++ 2021-peru-general-elections/lima-2021-ausentismo-electoral.R
@@ -0,0 +1,177 @@
+library(tidyverse)
+library(sf)
+library(patchwork)
+
+# Lima Departamento --------------------------------------------------------
+
+lima_dpto <- readRDS("2021-peru-general-elections/gadm36_PER_2_sf.rds") %>%
+  filter(
+    NAME_1 %in% c("Lima", "Lima Province")
+  )
+
+partic_lima_fn <- "2021-peru-general-elections/Participacion__LIMA_DETALLE_undefined.csv"
+
+
+upd_lbl <- read_lines(
+  file = partic_lima_fn,
+  skip = 5,
+  n_max = 1
+) %>%
+  str_remove_all('"') %>%
+  str_to_sentence()
+
+lima_ausentismo <- read_csv(
+  partic_lima_fn,
+  skip = 9
+) %>%
+  janitor::clean_names() %>%
+  select(
+    provincia, percent_total_ausentes
+  )
+
+lima_dpto_df <- lima_dpto %>%
+  mutate(
+    provincia = str_to_upper(NAME_2)
+  ) %>%
+  left_join(
+    lima_ausentismo,
+    by = "provincia"
+  )
+
+
+lima_map <- ggplot(lima_dpto_df) +
+  geom_sf(aes(fill = percent_total_ausentes),
+          color = "white") +
+  scale_fill_steps(
+    low = "#56B1F7",
+    high = "#132B43",
+    n.breaks = 5
+  ) +
+  labs(
+    fill = "Porcentaje\nde Ausentes"
+  ) +
+  theme_void(20) +
+  theme(
+    legend.key.height = unit(2, "line"),
+
+  )
+
+tabaus <- lima_ausentismo %>%
+  arrange(percent_total_ausentes) %>%
+  mutate(
+    percent_total_ausentes = sprintf("%.1f%%", percent_total_ausentes)
+  ) %>%
+  rename(
+    Provincia = 1,
+    "% Ausentes" = 2
+  )
+
+p_lima <- wrap_elements(gridExtra::tableGrob(tabaus)) + lima_map +
+  plot_layout(
+    widths = c(1, 2)
+  ) +
+  plot_annotation(
+    title = "Lima, Perú: Ausentismo en las Elecciones Generales 2021",
+    subtitle = glue::glue("Fuente: ONPE ({upd_lbl})"),
+    caption = "@jmcastagnetto, Jesus M. Castagnetto 2021-05-12"
+  ) &
+  theme(
+    plot.title = element_text(size = 20),
+    plot.caption = element_text(family = "Inconsolata", size = 16)
+  )
+
+ggsave(
+  plot = p_lima,
+  filename = "2021-peru-general-elections/lima-peru-2021-ausentismo-electoral.png",
+  width = 10,
+  height = 10
+)
+
+# Lima Provincia -------------------------------------------------------
+
+lima_prov <- readRDS("2021-peru-general-elections/gadm36_PER_3_sf.rds") %>%
+  filter(
+    NAME_1 %in% c("Lima Province")
+  )
+
+partic_limaprov_fn <- "2021-peru-general-elections/Participacion_LIMA_LIMA_DETALLE_undefined.csv"
+
+upd_lbl <- read_lines(
+  file = partic_limaprov_fn,
+  skip = 5,
+  n_max = 1
+) %>%
+  str_remove_all('"') %>%
+  str_to_sentence()
+
+limaprov_ausentismo <- read_csv(
+  partic_limaprov_fn,
+  skip = 9
+) %>%
+  janitor::clean_names() %>%
+  select(
+    distrito, percent_total_ausentes
+  )
+
+lima_prov_df <- lima_prov %>%
+  mutate(
+    distrito = str_to_upper(NAME_3),
+    distrito = if_else(
+      distrito == "MAGDALENA VIEJA",
+      "PUEBLO LIBRE",
+      distrito
+    )
+  ) %>%
+  left_join(
+    limaprov_ausentismo,
+    by = "distrito"
+  )
+
+limaprov_map <- ggplot(lima_prov_df) +
+  geom_sf(aes(fill = percent_total_ausentes),
+          color = "white") +
+  scale_fill_steps(
+    low = "#56B1F7",
+    high = "#132B43",
+    n.breaks = 9
+  ) +
+  labs(
+    fill = "Porcentaje\nde Ausentes"
+  ) +
+  theme_void(20) +
+  theme(
+    legend.key.height = unit(3, "line")
+  )
+
+tabaus <- limaprov_ausentismo %>%
+  arrange(percent_total_ausentes) %>%
+  mutate(
+    percent_total_ausentes = sprintf("%.1f%%", percent_total_ausentes)
+  ) %>%
+  rename(
+    Distrito = 1,
+    "% Ausentes" = 2
+  )
+
+p_limaprov <- wrap_elements(gridExtra::tableGrob(tabaus)) + limaprov_map +
+  plot_layout(
+    widths = c(1, 2)
+  ) +
+  plot_annotation(
+    title = "Lima Provincia, Lima, Perú: Ausentismo en las Elecciones Generales 2021",
+    subtitle = glue::glue("Fuente: ONPE ({upd_lbl})"),
+    caption = "@jmcastagnetto, Jesus M. Castagnetto 2021-05-12"
+  ) &
+  theme(
+    plot.title = element_text(size = 20),
+    plot.caption = element_text(family = "Inconsolata", size = 16)
+  )
+
+ggsave(
+  plot = p_limaprov,
+  filename = "2021-peru-general-elections/lima_provincia-peru-2021-ausentismo-electoral.png",
+  width = 12,
+  height = 14
+)
+
+
--- 2021-peru-general-elections/participación-ciudadana.R
+++ 2021-peru-general-elections/participación-ciudadana.R
@@ -18,56 +18,7 @@ partic_peru <- read_csv(
 ) %>%
   janitor::clean_names()
 
-# pct_peru <- partic_peru %>%
-#   select(
-#     departamento,percent_total_asistentes, percent_total_ausentes
-#   ) %>%
-#   mutate(
-#     departamento = factor(departamento) %>%
-#       fct_reorder(percent_total_asistentes)
-#   ) %>%
-#   pivot_longer(
-#     cols = 2:3,
-#     names_to = "metric",
-#     values_to = "value",
-#     names_prefix = "percent_total_"
-#   ) %>%
-#   mutate(
-#     metric = str_to_title(metric) %>%
-#       factor(
-#         levels = c("Ausentes", "Asistentes")
-#       ),
-#     xlbl = if_else(metric == "Asistentes", 30, 90)
-#   )
-#
-# ggplot(
-#   pct_peru,
-#   aes(y = departamento, x = value, fill = metric)
-# ) +
-#   geom_col() +
-#   geom_text(
-#     aes(label = sprintf("%.1f%%", value), x = xlbl),
-#     color = "white",
-#     fontface = "bold"
-#   ) +
-#   scale_fill_manual(
-#     values = c("Asistentes" = "#7570b3", "Ausentes" = "#d95f02")
-#   ) +
-#   labs(
-#     y = "",
-#     x = "Porcentaje de electores hábiles",
-#     fill = "",
-#     title = "Perú: Participación ciudadadana en las Elecciones Generales 2021",
-#     subtitle = glue::glue("Fuente: ONPE ({upd_lbl})"),
-#     caption = ""
-#   ) +
-#   theme_minimal(20) +
-#   theme(
-#     plot.title.position = "plot",
-#     legend.position = "bottom"
-#   )
-
-peru_map <- readRDS("~/Documents/datasets/peru-geodata/gadm_org/gadm36_PER_1_sf.rds")
+peru_map <- readRDS("2021-peru-general-elections/gadm36_PER_1_sf.rds")
 
 peru_df <- peru_map %>%
   mutate(
