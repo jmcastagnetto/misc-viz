9af8eb374194f18ebbc642976d2833f4453469ca
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-04-12 22:18:00 -0500

    Versión simplificada usando geom_sf_text()

--- 2021-peru-general-elections/presidenciales-partido-mas-votado-por-dpto.R
+++ 2021-peru-general-elections/presidenciales-partido-mas-votado-por-dpto.R
@@ -49,6 +49,11 @@ p1 <- ggplot(
     fill = guide_legend(nrow = 2)
   )
 
+
+
+
+
+
 tbl_df <- top1_dpto %>%
   select(departamento, partido, pct_validos) %>%
   mutate(
@@ -89,4 +94,47 @@ ggsave(
   filename = "2021-peru-general-elections/presidenciales_partido_mayor_voto_dpto.png",
   width = 17,
   height = 12
+)
+
+# simplificando
+
+p2 <- ggplot(
+  peru_partidos_df
+) +
+  geom_sf(aes(fill = partido), color = "white") +
+  geom_sf_text(
+    aes(label = sprintf("%.1f%%", pct_validos)),
+    check_overlap = TRUE,
+    size = 5,
+    fontface = "bold"
+  ) +
+  scale_fill_brewer(
+    palette = "Paired",
+    type = "qual"
+  ) +
+  labs(
+    title = "Perú: Elecciones Presidenciales 2021:\nPartido con mayor votación en cada Departamento",
+    subtitle = "Mostrando el porcentaje de votos válidos (Fuente: ONPE)",
+    caption = "@jmcastagnetto, Jesus M. Castagnetto 2021-05-12",
+    fill = ""
+  ) +
+  theme_void(18) +
+  theme(
+    legend.position = "bottom",
+    plot.title.position = "plot",
+    plot.title = element_text(size = 24),
+    plot.subtitle = element_text(size = 22, color = "grey50"),
+    plot.caption = element_text(family = "Inconsolata", size = 16),
+    plot.caption.position = "plot",
+    plot.margin = unit(rep(.5, 4), "cm")
+  ) +
+  guides(
+    fill = guide_legend(nrow = 2)
+  )
+p2
+ggsave(
+  plot = p2,
+  filename = "2021-peru-general-elections/presidenciales_partido_mayor_voto_dpto-simplificado.png",
+  height = 14,
+  width = 11
 )
